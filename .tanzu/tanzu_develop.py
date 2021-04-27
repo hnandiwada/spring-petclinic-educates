db20ccbcadcf420457a566ada0d8ce7ac0c5fd55
Harsha Nandiwada <hnandiwada@vmware.com>
2021-04-27 15:53:34 -0700

    File sync works but container does not restart because it doesn't have Devtools
    Co-authored-by: Harsha Nandiwada <hnandiwada@vmware.com>
    Co-authored-by: Paul Warren <warrenpa@vmware.com>

--- .tanzu/tanzu_develop.py
+++ .tanzu/tanzu_develop.py
@@ -1,16 +1,29 @@
+load('ext://local_output', 'local_output')
 # -*- mode: Python -*-
-
+# live reload
+def tanzu_develop(workload='', manifests=[], deps=["."], resource_deps=[], live_update=[]):
+    k8s_kind('Workload', api_version='experimental.kontinue.io/v1',
+            image_json_path='{.metadata.dev_image}')
+    k8s_resource(workload, port_forwards=8080,
+                resource_deps=resource_deps, 
+                extra_pod_selectors=[{'serving.knative.dev/service': workload}])
+    workload_yaml = local_output('kubectl get workloads ' + workload + ' -o yaml')
+    image_ref = local_output('kubectl get workloaddecorators ' + workload +'-workload-template -o jsonpath=\'{.status.template.spec.containers[?(@.name=="workload")].image}\'')
+    image_ref = image_ref.split("@sha256", 1)[0]
+    # add image to workload_yaml
+    workload_yaml = workload_yaml.replace("metadata:", "metadata:\n  dev_image: " + image_ref, 1)
+    print(workload_yaml)
+    manifests.append(blob(workload_yaml))
+    file_sync_only(image_ref, manifests, deps, live_update)
 # module function
 # non build development with only file sync
 def file_sync_only(image='', manifests=[], deps=["."], live_update=[]):
     if type(manifests) == "string":
         manifests = [manifests]
-
     # ex) image is "nginx" or "nginx:1.17" format
     # "nginx" => return "nginx" and "1.16" (from manifests)
     # "nginx:1.17" => return "nginx" and "1.17"
     deployed_image, deployed_tag = _get_current_tag(image, manifests)
-
     # ":" is null command
     custom_build(deployed_image, ':',
         tag=deployed_tag,
@@ -20,27 +33,22 @@ def file_sync_only(image='', manifests=[], deps=["."], live_update=[]):
     )
     k8s_yaml(manifests)
     _first_sync_from_liveupdate(deployed_image, live_update)
-
 # get current tags from image setting from first arg in file_sync_only() or in manifests
 def _get_current_tag(image='', manifests=[]):
     p = image.split(':')
     if len(p) == 2:
         return p[0], p[1]
-
     tags = []
-
     for m in manifests:
-        output = str(local(r"grep 'image: %s' %s | cut -d ':' -f 3 | uniq || true" % (image, m))).strip()
-        if (output != ''):
-            tags = tags + output.split("\n")
-
+        if type(m) == str:
+            output = str(local(r"grep 'image: %s' %s | cut -d ':' -f 3 | uniq || true" % (image, m))).strip()
+            if (output != ''):
+                tags = tags + output.split("\n")
     if len(tags) == 0:
         tags = ["latest"]
     elif len(tags) > 1:
         fail("found multiple image tags for %s in manifests %s" % (image, manifests))
-
     return image, tags[0]
-
 # sync files at first time
 def _first_sync_from_liveupdate(image, live_update):
     for l in live_update:
@@ -48,7 +56,6 @@ def _first_sync_from_liveupdate(image, live_update):
             local_path, remote_path = _get_sync_params(l)
             local_resource('%s-sync' % local_path,
                 'touch %s' % local_path)
-
 # return sync(local_path, remote_path)'s args
 def _get_sync_params(sync):
     output = str(sync).split("'")
--- Tiltfile
+++ Tiltfile
@@ -1,32 +1,27 @@
+load('.tanzu/tanzu_develop.py', 'tanzu_develop')
 allow_k8s_contexts('eduk8s')
-load('.tanzu/file_sync_only.py', 'file_sync_only')
 
 mvnw = "./mvnw"
 if os.name == "nt":
   mvnw = "mvnw.bat"
 
-#  './mvnw -Dmaven.test.skip=true -Dspring-boot.repackage.excludeDevtools=false package && rm -rf .build/* && unzip -o target/spring-petclinic-*.jar -d .build/',
 local_resource(
   'spring-petclinic-compile',
-  './mvnw -Dmaven.test.skip=true -Dspring-boot.repackage.excludeDevtools=false package && ' +
+  './mvnw -Dmaven.test.skip=true -Dspring-boot.repackage.excludeDevtools=false package && ' + 
     'rm -rf target/jar-staging && ' +
     'unzip -o target/spring-petclinic-*.jar -d target/jar-staging && ' +
     'rsync --delete --inplace --checksum -r target/jar-staging/ .build',
   deps=['src', 'pom.xml'])
 
-file_sync_only("harbor-repo.vmware.com/kontinuedemo/spring-petclinic",
-    ["./config/workload.yaml"],
+tanzu_develop(workload="spring-petclinic",
+    manifests=[],
     deps=['.build/'],
+    resource_deps=['spring-petclinic-compile'],
     live_update=[
       sync('.build/BOOT-INF/lib', '/workspace/BOOT-INF/lib'),
       sync('.build/META-INF', '/workspace/META-INF'),
       sync('.build/BOOT-INF/classes', '/workspace/BOOT-INF/classes'),
-      run("#TODO: figure out how to restart the container; i.e. something like 'kill -HUP 1' that actually works")
+      run("##TODO: figure out how to restart the container; i.e. something like 'kill -HUP 1' that actually works"),
+      run("#TODO: delete-me")
     ],
 )
-
-k8s_kind('Workload', pod_readiness='ignore')
-
-k8s_resource(workload='spring-petclinic', port_forwards=8080,
-             resource_deps=['spring-petclinic-compile'],
-             extra_pod_selectors=[{'app': 'spring-petclinic'}])
